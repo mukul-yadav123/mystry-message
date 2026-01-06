8b24d290f4eb79d8b9dea0d37b7015565d69bb6c
Mukul Yadav <mukuly2002@gmail.com>
2026-01-06 22:39:56 +0530

    options

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -56,5 +56,29 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt"
     },
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async session({session,token})
+        {
+            if(token)
+            {
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
+        },
+        async jwt({token,user}){
+            if(user)
+            {
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
+                token.username = user.username
+            }
+             
+            return token
+        }
+    }
 }
