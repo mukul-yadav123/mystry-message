986cf8887fcd15866e3d8af86f527dd012ddb204
Mukul Yadav <mukuly2002@gmail.com>
2026-01-08 22:15:29 +0530

    verify unique username

---
+++ app/api/check-username-unique/route.ts
@@ -0,0 +1,76 @@
+import {success, z} from "zod";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request) {
+    
+    if(request.method !== "GET")
+    {
+        return Response.json({
+            success: false,
+            message:"Method not allowed"
+        },{
+            status: 405
+        })
+    }
+    
+    await dbConnect()
+
+    try {
+        const {searchParams} = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get('username')
+        }
+
+        //validate with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        if(!result.success)
+        {
+            const usernameErrors = result.error.format().username?._errors || [];
+            return Response.json({
+                success: false,
+                message: usernameErrors
+            },
+        {
+            status: 400
+        })
+        }
+
+        const {username} = result.data
+        const existingVerifiedUser = await UserModel.findOne({
+            username,isVerified:true
+        })
+
+        if(existingVerifiedUser)
+            return Response.json({
+                success: false,
+                message: "Username already exists"
+            },
+        {
+            status: 400
+        })
+
+         return Response.json({
+                success: true,
+                message: "Username available"
+            },
+        {
+            status: 200
+        })
+
+    } catch (error) {
+        console.log('Error Checking username ',error)
+        return Response.json({
+            success: false,
+            message: "Error checking username"
+        },{
+            status: 500
+        })
+    }
+}
