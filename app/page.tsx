2d455bc5d0bce5f31b7263a326ab52b79f841dc5
Mukul Yadav <mukuly2002@gmail.com>
2026-01-23 18:50:13 +0530

    Mystry stabilized

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             Dive into the World of Anonymous Feedback
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            True Feedback - Where your identity remains a secret.
+            Mystry Message - Where your identity remains a secret.
           </p>
         </section>
 
@@ -57,7 +57,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © 2023 Mystry Message. All rights reserved.
       </footer>
     </>
   );
